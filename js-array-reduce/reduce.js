78018c5cd9229272465c9ba121078923e4ecad1f
Ziyi Zhang <ziyizhang@MacBook-Pro-2.lan1>
2021-12-05 15:01:16 -0500

    changes

--- js-array-reduce/reduce.js
+++ js-array-reduce/reduce.js
@@ -8,15 +8,10 @@ Examples:
 */
 
 function extractValue(arr, key) {
-    // arr.reduce(function(acc, next) {
-    //     return acc.push(next[key]);
-    //     return acc;
-    // });
-    let result = [];
-    arr.forEach(function(obj) {
-        result.push(obj[key]);
-    });
-    return result;
+    return arr.reduce(function(acc, next) {
+        acc.push(next[key]);
+        return acc;
+    }, []);
 }
 
 /*
@@ -30,7 +25,24 @@ Examples:
     vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
 */
 
-function vowelCount(str) {}
+function vowelCount(str) {
+    const arr = Array.from(str);
+    const vowel = 'aeiou';
+
+    return arr.reduce(function(acc, next) {
+        const newNext = next.toLowerCase();
+        if (vowel.indexOf(newNext) !== -1) {
+            if (acc[newNext]) {
+                acc[newNext]++;
+            } else {
+                acc[newNext] = 1;
+            }
+        }
+        return acc;
+    }, {})
+}
+
+
 
 /*
 Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
@@ -47,7 +59,14 @@ Examples:
        ]
 */
 
-function addKeyAndValue(arr, key, value) {}
+function addKeyAndValue(arr, key, value) {
+    return arr.reduce(function(acc, next, idx) {
+        acc[idx][key] = value;
+        return acc;
+    }, arr)
+}
+
+
 
 /*
 Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
