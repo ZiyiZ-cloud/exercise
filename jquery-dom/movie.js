9c3b8d57caf388203dcf5b98eb4d9f3b2806fa95
Ziyi Zhang <ziyizhang@MacBook-Pro-2.local>
2021-12-10 17:41:10 -0500

    movie app

--- jquery-dom/movie.js
+++ jquery-dom/movie.js
@@ -1,8 +1,23 @@
-$('h1').css('text-align', 'center');
-$('p').css('text-align', 'center')
+function addRating() {
+    let name = $('.form-control').eq(0).val();
+    let rate = $('.form-control').eq(1).val();
+    let list = `Movie "${name}" is rated: ${rate} out of 10`;
+    $('ol').append(`<li>${list} <button class='delete'>X</button></li>`);
+};
+
+function removeRating() {
+    $(document).on('click', '.delete', function() {
+        $(this).parent().remove();
+    })
+}
 
 
 
-$('.information').on('submit', function() {
-    $('ol').append('<li>New List</li>');
-});
+$('form').submit(function(e) {
+    e.preventDefault();
+
+    addRating();
+    removeRating();
+
+    $('input').val('');
+})
