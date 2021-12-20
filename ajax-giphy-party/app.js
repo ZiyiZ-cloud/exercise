6076ef9326e9bc4568d66c8595928953dab6d5cc
Ziyi Zhang <ziyizhang@MacBook-Pro-2.local>
2021-12-20 10:56:25 -0500

    1220

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -1,29 +1,39 @@
-async function addGifByKey(key) {
-    try {
-        const res = await axios.get(`https://api.giphy.com/v1/gifs/search`, {
-            params: {
-                q: words,
-                api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
-            }
-        });
-        console.log(res.data);
-        const img = document.createElement('img');
-        img.src = res.data.message;
-
-    } catch (e) {
-        alert('key not found!');
-        getRandomGif();
-    }
+function addGifByKey(key) {
+    const results = key.data.length;
+    const randomID = Math.floor(Math.random() * results);
+    const $newGif = $('<img>', { src: key.data[randomID].images.original.url });
+    const $gif = $('#gifs');
+    $gif.append($newGif)
 }
 
+// const form = document.querySelector('#search');
+// form.addEventListener('submit', async function(e) {
+//     e.preventDefault();
+//     const inputValue = document.getElementById('search').value;
+//     const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+//         params: {
+//             q: inputValue,
+//             api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+//         }
+//     });
+//     console.log(response.data);
+// })
 
-const form = document.querySelector('#search');
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    addGifByKey(input.value);
-    input.value = '';
-})
+$("form").on("submit", async function(evt) {
+    evt.preventDefault();
+    let inputValue = document.getElementById('search').value;
+    // let searchTerm = $searchInput.val();
+    // $searchInput.val("");
 
+    const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+        params: {
+            q: inputValue,
+            api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+        }
+    });
+    addGifByKey(response.data);
+    inputValue = null;
+});
 
 //remove all the gifs
 const result = document.getElementById('gifs');
