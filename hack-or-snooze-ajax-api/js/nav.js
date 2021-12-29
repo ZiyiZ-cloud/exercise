8c036c5ca6d3c027c60a75b9792a2480c72e2ded
Ziyi Zhang <ziyizhang@MacBook-Pro-2.local>
2021-12-29 18:40:53 -0500

    1229

--- hack-or-snooze-ajax-api/js/nav.js
+++ hack-or-snooze-ajax-api/js/nav.js
@@ -7,9 +7,9 @@
 /** Show main list of all stories when click site name */
 
 function navAllStories(evt) {
-  console.debug("navAllStories", evt);
-  hidePageComponents();
-  putStoriesOnPage();
+    console.debug("navAllStories", evt);
+    hidePageComponents();
+    putStoriesOnPage();
 }
 
 $body.on("click", "#nav-all", navAllStories);
@@ -17,10 +17,10 @@ $body.on("click", "#nav-all", navAllStories);
 /** Show login/signup on click on "login" */
 
 function navLoginClick(evt) {
-  console.debug("navLoginClick", evt);
-  hidePageComponents();
-  $loginForm.show();
-  $signupForm.show();
+    console.debug("navLoginClick", evt);
+    hidePageComponents();
+    $loginForm.show();
+    $signupForm.show();
 }
 
 $navLogin.on("click", navLoginClick);
@@ -28,9 +28,41 @@ $navLogin.on("click", navLoginClick);
 /** When a user first logins in, update the navbar to reflect that. */
 
 function updateNavOnLogin() {
-  console.debug("updateNavOnLogin");
-  $(".main-nav-links").show();
-  $navLogin.hide();
-  $navLogOut.show();
-  $navUserProfile.text(`${currentUser.username}`).show();
+    console.debug("updateNavOnLogin");
+    $(".main-nav-links").show();
+    $navLogin.hide();
+    $navLogOut.show();
+    $navUserProfile.text(`${currentUser.username}`).show();
 }
+
+function showSubmit() {
+    const sumbitform = document.getElementById('submit-form');
+    if (sumbitform.style.display === 'none') {
+        sumbitform.style.display = 'flex';
+    } else {
+        sumbitform.style.display = 'none';
+    }
+}
+
+$('#navSubmit').on('click', showSubmit);
+
+/** Show favorite stories on click on "favorites" */
+
+function navFavoritesClick(evt) {
+    console.debug("navFavoritesClick", evt);
+    hidePageComponents();
+    putFavoritesListOnPage();
+}
+
+$body.on("click", "#nav-favorites", navFavoritesClick);
+
+/** Show My Stories on clicking "my stories" */
+
+function navMyStories(evt) {
+    console.debug("navMyStories", evt);
+    hidePageComponents();
+    putUserStoriesOnPage();
+    $ownStories.show();
+}
+
+$body.on("click", "#nav-mystories", navMyStories);
